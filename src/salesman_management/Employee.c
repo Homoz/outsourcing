39c2fd264934be1d2c090817d39f0867904f1ad2
forest0 <forest9643@gmail.com>
2017-09-22 23:22:23 +0800

    remove defination for Employee.toString

--- src/salesman_management/Employee.c
+++ src/salesman_management/Employee.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-const char *toString(const Employee &emplyee) {
-  char *name = emplyee.name;
-  char *age;
-  itoa(emplyee.age, age, 10);
-  char sex;
-  if (emplyee.sex = 0) {
-    sex = "女";
-  } else {
-    sex = "男";
-  }
-
-  char *reslut = malloc()
-}
+/* const char *toString(const Employee &emplyee) { */
+/*   char *name = emplyee.name; */
+/*   char *age; */
+/*   itoa(emplyee.age, age, 10); */
+/*   char sex; */
+/*   if (emplyee.sex = 0) { */
+/*     sex = "女"; */
+/*   } else { */
+/*     sex = "男"; */
+/*   } */
+/*  */
+/*   char *reslut = malloc() */
+/* } */
