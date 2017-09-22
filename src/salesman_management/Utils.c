7741eedea217c77b724e45e42775fa2d12b29683
forest0 <forest9643@gmail.com>
2017-09-22 23:20:43 +0800

    add logger for Utils

--- src/salesman_management/Utils.c
+++ src/salesman_management/Utils.c
@@ -1,23 +1,43 @@
 #include <memory.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "Utils.h"
 #include "Employee.h"
+#include "Utils.h"
+
+const char *RECORD_FORMAT = "%s %d %d %s %lf %s %s %d\n";
 
 void employeeToStr(char **dest, struct Employee *employee) {
     unsigned int size = 256;
     *dest = (char *)malloc(size);
     memset(*dest, 0, size);
-    snprintf(*dest, size, "%s %d %d %s %lf %s %s %d", 
-            employee->name, employee->age, employee->sex, employee->education,
-            employee->salary, employee->address, employee->phoneNumber, employee->isValid);
+    snprintf(*dest, size, RECORD_FORMAT, 
+            employee->name, employee->age, employee->sex, 
+            employee->education, employee->salary, 
+            employee->address, employee->phoneNumber, 
+            employee->isValid);
 }
 
 void strToEmployee(const char *str, struct Employee **employee) {
     *employee = (struct Employee *)malloc(sizeof(struct Employee));
     memset(*employee, 0, sizeof(struct Employee));
-    sscanf(str, "%s %d %d %s %lf %s %s %d",
-            (*employee)->name, &((*employee)->age), &((*employee)->sex),
-            (*employee)->education,&((*employee)->salary),(*employee)->address,
-            (*employee)->phoneNumber,&((*employee)->isValid));
+    sscanf(str, RECORD_FORMAT,
+            (*employee)->name, &((*employee)->age), 
+            &((*employee)->sex), (*employee)->education,
+            &((*employee)->salary), (*employee)->address,
+            (*employee)->phoneNumber, &((*employee)->isValid));
+}
+
+void logger(char type, const char *msg) {
+    switch (type) {
+        case 'i':
+            fprintf(stderr, "info: %s\n", msg);
+            break;
+        case 'w':
+            fprintf(stderr, "warning: %s\n", msg);
+            break;
+        case 'e':
+            fprintf(stderr, "error: %s\n", msg);
+        default:
+            fprintf(stderr, "default: %s\n", msg);
+    }
 }
--- src/salesman_management/Utils.h
+++ src/salesman_management/Utils.h
@@ -1,9 +1,14 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include "Employee.h"
+
 // 营销员转化为字符串
-void employeeToStr(char **dest, struct Employee *employee);
+void employeeToStr(char **, struct Employee *);
 // 字符串构造营销员
-void strToEmployee(const char *str, struct Employee **employee);
+void strToEmployee(const char *, struct Employee **);
+
+// 报错函数
+void logger(char, const char *);
 
 #endif
