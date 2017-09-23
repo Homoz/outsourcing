35341ddb045ce2f04d85110945eb8aa143c9b2fc
forest0 <forest9643@gmail.com>
2017-09-23 16:32:52 +0800

    add init and finit for Management

--- src/salesman_management/Management.c
+++ src/salesman_management/Management.c
@@ -1,16 +1,30 @@
-#include "Management.h"
-#include "Emplyee.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include "Employee.h"
+#include "Archive.h"
+#include "Management.h"
 
 // run
 void run();
 //增加
-void addEmployee(Employee *employee) { printf("请输入姓名:"); }
+void addEmployee(struct Employee *employee) {
+    printf("请输入姓名:"); 
+}
 //删除
-void removeEmployee(Employee *);
+void removeEmployee(struct Employee *);
 //查找
-void selectEmployee(Employee *);
+void selectEmployee(struct Employee *);
 //更新
-void updateEmployee(Employee *);
+void updateEmployee(struct Employee *);
 //打印all
 void printAll();
+
+void init() {
+    g_arch = (struct Archive *)malloc(
+            sizeof(struct Archive));
+    initArchive(g_arch, "data/archive.txt");
+}
+
+void finit() {
+    finitArchive(g_arch);
+}
--- src/salesman_management/Management.h
+++ src/salesman_management/Management.h
@@ -1,18 +1,27 @@
 #ifndef MANAGEMENT_H
 #define MANAGEMENT_H
-#include <Emplyee.h>
+
+#include "Employee.h"
+
+// 唯一全局档案
+struct Archive *g_arch;
 
 // run
 void run();
 //增加
-void addEmployee(Employee *);
+void addEmployee(struct Employee *);
 //删除
-void removeEmployee(Employee *);
+void removeEmployee(struct Employee *);
 //查找
-void selectEmployee(Employee *);
+void selectEmployee(struct Employee *);
 //更新
-void updateEmployee(Employee *);
+void updateEmployee(struct Employee *);
 //打印all
 void printAll();
+
+// 初始化
+void init();
+// 扫尾工作
+void finit();
 
 #endif
