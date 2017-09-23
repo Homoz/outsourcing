e726205770a88325a7edec818d4d89fb1d4ba2a1
forest0 <forest9643@gmail.com>
2017-09-23 16:32:24 +0800

    correct misspell from emplyee to employee

--- src/salesman_management/Employee.c
+++ src/salesman_management/Employee.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* const char *toString(const Employee &emplyee) { */
-/*   char *name = emplyee.name; */
+/* const char *toString(const Employee &employee) { */
+/*   char *name = employee.name; */
 /*   char *age; */
-/*   itoa(emplyee.age, age, 10); */
+/*   itoa(employee.age, age, 10); */
 /*   char sex; */
-/*   if (emplyee.sex = 0) { */
+/*   if (employee.sex = 0) { */
 /*     sex = "女"; */
 /*   } else { */
 /*     sex = "男"; */
