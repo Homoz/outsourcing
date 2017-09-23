62654045a0c174068fdfd95983454bbf50b9ebfc
forest0 <forest9643@gmail.com>
2017-09-23 10:44:32 +0800

    init and finit for archive, find and delete by name for archive

--- src/salesman_management/Archive.c
+++ src/salesman_management/Archive.c
@@ -1,19 +1,22 @@
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
 #include "Utils.h"
 #include "Archive.h"
 
-void init(struct Archive *, const char *path);
+void initArchive(struct Archive *, const char *path);
+void finitArchive(struct Archive *);
 void add(struct Archive *, struct Employee *);
 void update(struct Archive *, struct Employee *);
-void delete(struct Archive *, struct Employee *);
-void find(struct Archive *, struct Employee *);
+void deleteByName(struct Archive *, const char *);
+void findByName(struct Archive *, const char *, 
+        struct Employee **);
 void save(struct Archive *);
 
 void initArchive(struct Archive *arch, const char *path) {
     arch->MAX_BUFFER_SIZE = 64;
     if (strlen(path) >= arch->MAX_BUFFER_SIZE) {
-        fprintf(stderr, "error: exceed max buffer size");
+        /* fprintf(stderr, "error: exceed max buffer size"); */
         logger('e', "exceed max buffer size");
         exit(1);
     }
@@ -21,23 +24,50 @@ void initArchive(struct Archive *arch, const char *path) {
     for (; i < strlen(path); ++i) {
         arch->path[i] = path[i];
     }
+    /* TODO: 文件不存在时会报错 */
     arch->archiveFile = fopen(arch->path, "r+");
     if (!arch->archiveFile) {
-        fprintf(stderr, "error: failed to open %s\n", 
-                arch->path);
-        // TODO: 指出文件名
-        logger('e', "failed to open data file");
+        logger('e', strerror(errno));
         exit(1);
     }
 }
 
+void finitArchive(struct Archive *arch) {
+    if (arch->archiveFile) {
+        if (fclose(arch->archiveFile)) { // 文件关闭出错
+            logger('e', strerror(errno));
+        }
+    } else {
+        logger('w', "no open file, so not close");
+    }
+}
+
 // 文件结尾追加一条记录
-void add(struct Archive *arch, struct Employee *emplyee) {
-    // 设置文件读写头
+void add(struct Archive *arch, struct Employee *employee) {
+    // 设置文件读写头到文件结尾
     fseek(arch->archiveFile, 0, SEEK_END);
     char *buffer = NULL;
-    employeeToStr(&buffer, emplyee);
+    employeeToStr(&buffer, employee);
     fprintf(arch->archiveFile, "%s", buffer);
     free(buffer);
+}
 
+// 调用者需要释放 employee
+void findByName(struct Archive *arch, const char *name, 
+        struct Employee **employee) {
+    // 设置文件读写头到文件开头
+    fseek(arch->archiveFile, 0, SEEK_SET);
+    char buffer[256];
+    struct Employee *iter = NULL;
+    while (!feof(arch->archiveFile) 
+            && fgets(buffer, 255, arch->archiveFile)) {
+        
+        strToEmployee(buffer, &iter);
+        if (!strcmp(name, iter->name)) { // 匹配到
+            *employee = iter;
+            return;
+        }
+        free(iter);
+    }
+    *employee = NULL; // 未匹配到
 }
--- src/salesman_management/Archive.h
+++ src/salesman_management/Archive.h
@@ -11,10 +11,12 @@ struct Archive {
 };
 
 void initArchive(struct Archive *, const char *);
+void finitArchive(struct Archive *);
 void add(struct Archive *, struct Employee *);
 void update(struct Archive *, struct Employee *);
-void delete(struct Archive *, struct Employee *);
-void find(struct Archive *, struct Employee *);
+void deleteByName(struct Archive *, const char *);
+void findByName(struct Archive *, const char *, 
+        struct Employee **);
 void save(struct Archive *);
 
 #endif
