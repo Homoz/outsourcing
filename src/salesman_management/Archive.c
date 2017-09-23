7568f24a0552d16d48a78315a6ca5d923e282f88
forest0 <forest9643@gmail.com>
2017-09-23 14:46:21 +0800

    deleteByName function for Archive complete

--- src/salesman_management/Archive.c
+++ src/salesman_management/Archive.c
@@ -8,8 +8,8 @@ void initArchive(struct Archive *, const char *path);
 void finitArchive(struct Archive *);
 void add(struct Archive *, struct Employee *);
 void update(struct Archive *, struct Employee *);
-void deleteByName(struct Archive *, const char *);
-void findByName(struct Archive *, const char *, 
+int deleteByName(struct Archive *, const char *);
+int findByName(struct Archive *, const char *, 
         struct Employee **);
 void save(struct Archive *);
 void printAllValid(struct Archive *);
@@ -54,7 +54,7 @@ void add(struct Archive *arch, struct Employee *employee) {
 }
 
 // 调用者需要释放 employee
-void findByName(struct Archive *arch, const char *name, 
+int findByName(struct Archive *arch, const char *name, 
         struct Employee **employee) {
     // 设置文件读写头到文件开头
     fseek(arch->archiveFile, 0, SEEK_SET);
@@ -66,15 +66,34 @@ void findByName(struct Archive *arch, const char *name,
         strToEmployee(buffer, &iter);
         if (iter->isValid && !strcmp(name, iter->name)) { // 匹配到
             *employee = iter;
-            return;
+            return 0;
         }
         free(iter);
     }
     *employee = NULL; // 未匹配到
+    return 1;
 }
 
-void deleteByName(struct Archive *arch, const char *name) {
+int deleteByName(struct Archive *arch, const char *name) {
+    // 设置文件读写头到文件开头
+    fseek(arch->archiveFile, 0, SEEK_SET);
+    char buffer[256];
+    struct Employee *iter = NULL;
+    while (!feof(arch->archiveFile)
+            && fgets(buffer, 255, arch->archiveFile)) {
 
+        strToEmployee(buffer, &iter);
+        if (iter->isValid && !strcmp(name, iter->name)) { // 匹配到
+            size_t recodeLength = strlen(buffer);
+            // 设置文件读写头到本条记录开始
+            fseek(arch->archiveFile, -recodeLength, SEEK_CUR);
+            // 设置记录中的是否有效字段为无效
+            fprintf(arch->archiveFile, "%d", 0);
+            return 0;
+        }
+        free(iter);
+    }
+    return 1; // 无 name 对应的有效记录, 删除失败
 }
 
 void printAllValid(struct Archive *arch) {
